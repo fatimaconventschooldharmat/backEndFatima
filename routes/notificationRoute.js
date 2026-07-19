c17ac5064ad2239f0a02e32f88d3cd93a91ec57f
fatimaconventschooldharmat <fatimaconventschooldharmat@gmail.com>
2026-07-19 12:19:53 +0530

    Update notificationRoute.js

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import { upload } from '../configs/multer.js';
 import { getNotifications, markAsRead, createNotification, deleteNotification, uploadAttachment } from '../controllers/notificationController.js';
-import authUser from '../middleware/authUser.js';
+import authOptionalUser from '../middleware/authOptionalUser.js';
+import authSellerOrUser from '../middleware/authSellerOrUser.js';
 
 const notificationRouter = express.Router();
 
-notificationRouter.get('/get', authUser, getNotifications);
-notificationRouter.put('/read/:id', authUser, markAsRead);
+notificationRouter.get('/get', authOptionalUser, getNotifications);
+notificationRouter.put('/read/:id', authOptionalUser, markAsRead);
 // upload single file
-notificationRouter.post('/upload', authUser, upload.single('file'), uploadAttachment);
-notificationRouter.post('/create', authUser, createNotification);
-notificationRouter.delete('/delete/:id', authUser, deleteNotification);
+notificationRouter.post('/upload', authSellerOrUser, upload.single('file'), uploadAttachment);
+notificationRouter.post('/create', authSellerOrUser, createNotification);
+notificationRouter.delete('/delete/:id', authSellerOrUser, deleteNotification);
 
 export default notificationRouter;
